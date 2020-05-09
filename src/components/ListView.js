af0aa6db76225bf0dcad770501aa2a4c30c1021f
mazerunnerzong <hozongsien@gmail.com>
2020-05-09 16:10:00 +0800

    Added scroll to top button

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import ListItem from '@material-ui/core/ListItem';
+import { Container, ListItem, Fab, Fade } from '@material-ui/core';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import { FixedSizeList } from 'react-window';
 import ImgCard from './ImgCard';
 
@@ -9,20 +10,26 @@ const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
     height: '100%',
-    minHeight: '600px',
     backgroundColor: theme.palette.background.paper,
     padding: '0',
     margin: '0',
     display: 'flex',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
   },
   list: {
     backgroundColor: theme.palette.background.paper,
+    scrollBehavior: 'smooth',
+  },
+  scrollUp: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
   },
 }));
 
-function renderRow(props) {
+const renderRow = (props) => {
   const { index, style } = props;
 
   return (
@@ -30,7 +37,7 @@ function renderRow(props) {
       <ImgCard title="Title" image="/pics/reptile.jpg" description="Descrition" alt="alt" />
     </ListItem>
   );
-}
+};
 
 renderRow.propTypes = {
   index: PropTypes.number.isRequired,
@@ -39,13 +46,45 @@ renderRow.propTypes = {
 
 const ListView = () => {
   const classes = useStyles();
+  const listRef = React.useRef();
+  const [isScrollForward, setScrollForward] = React.useState(false);
+
+  const handleClickToScrollTop = () => {
+    listRef.current.scrollToItem(0);
+  };
+
+  const handleScroll = (event) => {
+    if (event.scrollDirection === 'backward') {
+      setScrollForward(true);
+    } else {
+      setScrollForward(false);
+    }
+  };
 
   return (
-    <div className={classes.root}>
-      <FixedSizeList className={classes.list} height={600} itemCount={5} itemSize={300} width={350}>
+    <Container className={classes.root}>
+      <FixedSizeList
+        className={classes.list}
+        ref={listRef}
+        onScroll={handleScroll}
+        height={650}
+        itemCount={10}
+        itemSize={300}
+        width={350}
+      >
         {renderRow}
       </FixedSizeList>
-    </div>
+      <Fade in={isScrollForward}>
+        <Fab
+          className={classes.scrollUp}
+          color="primary"
+          aria-label="up"
+          onClick={handleClickToScrollTop}
+        >
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </Fade>
+    </Container>
   );
 };
 
