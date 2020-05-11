25627b533c466daa04ac59c7c614466085dd3e95
mazerunnerzong <hozongsien@gmail.com>
2020-05-11 14:46:20 +0800

    Updates

---
+++ precache-manifest.18226e5f255f33f5e5b7b97968035b10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d2c42494bcca8f6b40481c924c761c",
+    "url": "/Imposter/index.html"
+  },
+  {
+    "revision": "c1da2577856ce0355e52",
+    "url": "/Imposter/static/css/main.4289beb9.chunk.css"
+  },
+  {
+    "revision": "6ff5de7114f28bcc0cf9",
+    "url": "/Imposter/static/js/2.6fe784d5.chunk.js"
+  },
+  {
+    "revision": "e46609aec777f9260d21834116f9e6de",
+    "url": "/Imposter/static/js/2.6fe784d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1da2577856ce0355e52",
+    "url": "/Imposter/static/js/main.f0500b86.chunk.js"
+  },
+  {
+    "revision": "ec7f30271715a2f7039c",
+    "url": "/Imposter/static/js/runtime-main.5d73dc0d.js"
+  }
+]);
