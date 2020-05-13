cefc597c15ae1cb42cbf0ab463463733666ebbb6
mazerunnerzong <hozongsien@gmail.com>
2020-05-13 20:13:22 +0800

    Add schema for basic query and mutation of video info

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,59 @@
-import express from 'express';
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const videos = [];
+
+const schema = buildSchema(`
+  type Video {
+    _id: ID!
+    title: String!
+    authorName: String!
+    url: String!
+  }
+
+  input VideoInput {
+    title: String!
+    authorName: String!
+    url: String!
+  }
+
+  type RootQuery {
+      videos: [Video!]!
+  }
+  type RootMutation {
+      createVideo(videoInput: VideoInput): Video
+  }
+  schema {
+      query: RootQuery
+      mutation: RootMutation
+  }
+`);
+
+const rootValue = {
+  videos: () => {
+    return videos;
+  },
+  createVideo: (args) => {
+    const video = {
+      _id: Math.random().toString(),
+      title: args.videoInput.title,
+      authorName: args.videoInput.authorName,
+      url: args.videoInput.url,
+    };
+    videos.push(video);
+    return video;
+  },
+};
 
 const server = express();
+server.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue,
+    graphiql: true,
+  })
+);
 
-server.use(express.json());
-
-server.get('/', (req, res, next) => {
-  res.send('Hello World');
-});
-
-server.listen(5000);
+server.listen(5000, () => console.log('Now browse to localhost:5000/graphql'));
